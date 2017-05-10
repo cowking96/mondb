bb556d5d13307afdb1cf0df54a190142583f3b02
cowking96 <kakson96@yahoo.com>
2017-05-09 20:13:03 -0400

    Adds springboot core functionality

---
+++ src/main/java/com/cowking96/MondbApplication.java
@@ -0,0 +1,12 @@
+package com.cowking96;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MondbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MondbApplication.class, args);
+	}
+}
