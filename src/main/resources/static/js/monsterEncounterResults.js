c40be225bb998e25a83e6b7acfbe5d0aab66cf43
cowking96 <kakson96@yahoo.com>
2017-07-11 15:35:21 -0400

    finishes implementing front end for encounter builder

--- src/main/resources/static/js/monsterEncounterResults.js
+++ src/main/resources/static/js/monsterEncounterResults.js
@@ -1,6 +1,8 @@
 EncounterSearchResults = {
 
     table: {},
+    xpTableFinal: {},
+
 
     startup: function() {
 
@@ -14,6 +16,10 @@ EncounterSearchResults = {
                 { data: 'type'}
             ]
         } );
+
+        this.xpTableFinal = $('#encounterSearchResultsXpTableHolder').DataTable( {
+                    "order": [[ 2, "desc" ]]
+                } );
     },
 
     buildPage: function(data) {
@@ -22,7 +28,102 @@ EncounterSearchResults = {
 
         var monsterList = data.encounterMonsterList
 
-        this.table.clear().rows.add(monsterList).draw();
+        var xpList = data.xpTable;
 
+        this.table.clear().rows.add(monsterList).draw();
+        this.xpTableFinal.clear();
+
+        for(i = 1; i <= 6;i++){
+
+
+        intermediateTable = [];
+        xpTableFinal = [];
+
+
+
+        switch(i){
+
+        case 1:
+
+            intermediateTable.push("1 monster");
+
+            for (j = 1; j <=4;j++){
+                intermediateTable[j] = xpList[0][j-1]
+            }
+
+            xpTableFinal.push(intermediateTable);
+            console.log(intermediateTable);
+            this.xpTableFinal.rows.add(xpTableFinal).draw();
+            break;
+
+
+        case 2:
+
+            intermediateTable.push("2 monsters");
+
+            for (j = 1; j <=4;j++){
+                intermediateTable[j] = xpList[1][j-1]
+            }
+
+            xpTableFinal.push(intermediateTable);
+            console.log(intermediateTable);
+            this.xpTableFinal.rows.add(xpTableFinal).draw();
+            break;
+
+        case 3:
+
+            intermediateTable.push("3-6 monsters");
+
+            for (j = 1; j <=4;j++){
+                intermediateTable[j] = xpList[2][j-1]
+            }
+
+            xpTableFinal.push(intermediateTable);
+            console.log(intermediateTable);
+            this.xpTableFinal.rows.add(xpTableFinal).draw();
+            break;
+
+        case 4:
+
+            intermediateTable.push("7-10 monsters");
+
+            for (j = 1; j <=4;j++){
+                intermediateTable[j] = xpList[3][j-1]
+            }
+            xpTableFinal.push(intermediateTable);
+            console.log(intermediateTable);
+            this.xpTableFinal.rows.add(xpTableFinal).draw();
+            break;
+
+        case 5:
+
+            intermediateTable.push("11-14 monsters");
+
+            for (j = 1; j <=4;j++){
+                intermediateTable[j] = xpList[4][j-1]
+            }
+            xpTableFinal.push(intermediateTable);
+            console.log(intermediateTable);
+            this.xpTableFinal.rows.add(xpTableFinal).draw();
+            break;
+
+        case 6:
+
+        intermediateTable.push("15+ monsters");
+
+        for (j = 1; j <=4;j++){
+            intermediateTable[j] = xpList[5][j-1]
+        }
+
+        xpTableFinal.push(intermediateTable);
+        console.log(intermediateTable);
+        this.xpTableFinal.rows.add(xpTableFinal).draw();
+        break;
     }
+
+
+   }
+
+//   this.xpTableFinal.clear().rows.add(xpList).draw();
+}
 }
